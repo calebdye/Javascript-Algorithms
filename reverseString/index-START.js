5f06b760ab97aba81350b1053fe5dcddd9fe1d54
caleb <calebemorydye@gmail.com>
2023-03-05 20:39:06 -0700

    Basic tests

--- reverseString/index-START.js
+++ reverseString/index-START.js
@@ -1,6 +1,23 @@
 
+//for/for of/for each approach
 function reverseString(text) {
-    // add your code here
+    let result = ''
+    for(let char of text){
+        result = char = result
+    }
+    return result
+}
+//recursion approach
+function reverseString(text) {
+if (text===''){
+    return ''
+} else {
+    return reverseString(text.substr(1))+text[0]
+}
 }
 
+//reduce
+function reverseString(text) {
+return [...text].reduce((a,b)=> b + a,'')
+}
 module.exports = reverseString;
--- vowelsCounter/index-START.js
+++ vowelsCounter/index-START.js
@@ -1,6 +1,23 @@
 
+// function vowelsCounter(text) {
+//     let vowels = ['a','e','i','o','u']
+//     let count = 0
+//     for(letter of text.toLowerCase()){
+//         if(vowels.includes(letter)){
+//             count++
+//         }
+        
+//     } 
+//     return count
+// }
+
 function vowelsCounter(text) {
-    // add your code here
+    let count = text.match(/[aeiou]/gi);
+    if(count){
+        return count.length
+    }else {
+        return 0
+    }
 }
 
 module.exports = vowelsCounter;
