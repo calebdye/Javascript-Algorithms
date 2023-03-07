ea43d459f64ae5d186e6af6f772e60105c94daa4
caleb <calebemorydye@gmail.com>
2023-03-06 21:00:22 -0700

    creating solutions for highest char in string

--- maxReccuringChar/index-START.js
+++ maxReccuringChar/index-START.js
@@ -1,6 +1,29 @@
 
-function maxRecurringChar(text) {
-    // add your code here
-}
+// function maxRecurringChar(text) {
+//   let arr =  text.split('')
+//   let obj = arr.map()
+//   console.log(obj)
+// }
 
+
+// Formating arrays
+function maxRecurringChar(text) {
+    let charMap = {}
+    let charArray =[]
+    let vaulesArray = []
+    let maxCharValue = 0
+
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+    charArray = Object.keys(charMap)
+    vaulesArray = Object.values(charMap)
+    maxCharValue = Math.max(...vaulesArray)
+    
+    return charArray[vaulesArray.indexOf(maxCharValue)]
+}
 module.exports = maxRecurringChar;
