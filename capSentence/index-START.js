ec1ae40eeda4dc85db8979cc3b0128626ef59d12
caleb <calebemorydye@gmail.com>
2023-03-09 12:42:20 -0700

    Making algos for the given tests

--- capSentence/index-START.js
+++ capSentence/index-START.js
@@ -1,6 +1,30 @@
 
+// function capSentence(text) {
+//     let words = text.toLowerCase().split(' ')
+//     let caps = []
+//     words.forEach((word)=> {
+//         caps.push(word[0].toUpperCase() + word.slice(1))
+//     })
+//     return caps.join(' ')
+// }
+ 
+
+// function capSentence(text) {
+//     let words = text.toLowerCase().split(' ')
+//     let caps = words.map((word)=> {
+//         return word[0].toUpperCase() + word.slice(1)
+//     })
+    
+//     return caps.join(' ')
+// }
+
 function capSentence(text) {
-    // add your code here
+    let words = text.toLowerCase().split(' ')
+    let caps = words.map((word)=> {
+        return word.replace(word[0],word[0].toUpperCase())
+    })
+    
+    return caps.join(' ')
 }
  
 module.exports = capSentence;
