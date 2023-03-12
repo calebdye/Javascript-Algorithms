8409c18f673579bdf2b033eeabd709eb96e45094
caleb <calebemorydye@gmail.com>
2023-03-12 13:55:30 -0600

    palindromes solutions

--- palindromeChecker/index-START.js
+++ palindromeChecker/index-START.js
@@ -1,6 +1,37 @@
 
+// function palindromeChecker(text) {
+//     let rev = text.split('').reverse().join('')
+
+//     return rev === text
+
+// }
+
+
+// function palindromeChecker(text) {
+//     let rev = text.split('')
+
+//     let result = rev.every((letter, index)=>{
+//         return letter === rev[rev.length - index -1]
+//     })
+//  return result
+// }
+
+
 function palindromeChecker(text) {
-    // add your code here
+    let rev = text.split('')
+
+    for(let i=0;i<rev.length/2;i++){
+        
+        if(text[i] !== text[text.length - 1 -i]){
+            return false
+        }
+
+    }
+
+    // let result = rev.every((letter, index)=>{
+    //     return letter === rev[rev.length - index -1]
+    // })
+ return true
 }
 
 module.exports = palindromeChecker;
